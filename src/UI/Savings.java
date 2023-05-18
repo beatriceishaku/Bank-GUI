774ea5d00d10d7bc0727138bfbb7757714bbf6b1
OnigbodeNathan <131052547+OnigbodeNathan@users.noreply.github.com>
2023-05-18 09:36:19 +0100

    g update

---
+++ src/UI/Savings.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Savings {
+    private static final int initialAmount = 100000;
+    private static final int maximumAmount = 20000;
+    public class withdrawal{
+        Scanner input = new Scanner(System.in);
+        int withdrawalAmount = input.nextInt();
+
+    }
+}
