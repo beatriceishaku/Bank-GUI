fa8ae191895d711927858d06a748b43b7c2bb0d9
Shadrach7 <132397952+Shadrach7@users.noreply.github.com>
2023-05-19 07:50:44 -0700

    Update Savings.java

--- src/UI/Savings.java
+++ src/UI/Savings.java
@@ -8,7 +8,6 @@ import java.time.format.DateTimeFormatter;
 
 public class Savings {
     public static final String FILE_PATH = "savings.txt";
-    public static final String FILE_PATH = "savings.txt";
     public static final int AMOUNT = 100000;
     private static final int MAXIMUM_AMOUNT = 20000;
 
