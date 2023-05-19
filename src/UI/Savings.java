7828e72d1258aa90446cf47a1ec7c9c9380db1ba
Shadrach7 <132397952+Shadrach7@users.noreply.github.com>
2023-05-19 07:55:35 -0700

    Update Savings.java

--- src/UI/Savings.java
+++ src/UI/Savings.java
@@ -1,37 +1,45 @@
 package UI;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
 public class Savings {
     public static final String FILE_PATH = "savings.txt";
     public static final int AMOUNT = 100000;
     private static final int MAXIMUM_AMOUNT = 20000;
 
+    public static void main(String[] args) {
+        // Example usage
+        saveAccountData("John Doe", 5000);
+    }
+
     public static void saveAccountData(String accountHolder, int balance) {
         try (FileWriter writer = new FileWriter(FILE_PATH)) {
-            // Write the account data to the file
+            // Get the current date and time
+            LocalDateTime dateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+            // Write the account data with date and time to the file
             writer.write("Account Holder: " + accountHolder + "\n");
             writer.write("Balance: " + balance + "\n");
             writer.write("Date and Time: " + dateTime.format(formatter) + "\n");
-
 
             System.out.println("Account data saved successfully.");
         } catch (IOException e) {
             System.out.println("An error occurred while saving account data: " + e.getMessage());
         }
+    }
 
-    class WithdrawalAmount {
-            Scanner input = new Scanner(System.in);
-            int withdrawalAmount;
+    public class WithdrawalAmount {
+        Scanner input = new Scanner(System.in);
+        int withdrawalAmount;
 
-            public WithdrawalAmount() {
-                withdrawalAmount = input.nextInt();
+        public WithdrawalAmount() {
+            withdrawalAmount = input.nextInt();
             // Add validation and error handling for withdrawalAmount here
-            }
         }
     }
-}    
+}
