ed043f5a35a0e0e83b1f0fb4c721687548e4e01c
Shadrach7 <132397952+Shadrach7@users.noreply.github.com>
2023-05-19 07:39:31 -0700

    Update Savings.java

--- src/UI/Savings.java
+++ src/UI/Savings.java
@@ -1,10 +1,23 @@
 package UI;
-
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Savings {
+    public static final String FILE_PATH = "savings.txt";
     public static final int AMOUNT = 100000;
     private static final int MAXIMUM_AMOUNT = 20000;
+
+    public static void saveAccountData(String accountHolder, int balance) {
+        try (FileWriter writer = new FileWriter(FILE_PATH)) {
+            // Write the account data to the file
+            writer.write("Account Holder: " + accountHolder + "\n");
+            writer.write("Balance: " + balance + "\n");
+
+            System.out.println("Account data saved successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving account data: " + e.getMessage());
+        }
 
     public class WithdrawalAmount {
         Scanner input = new Scanner(System.in);
