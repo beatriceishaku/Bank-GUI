535a6e0983e0bb11ac4fa50d185c132f36272915
Emmanuelowuno <132396131+Emmanuelowuno@users.noreply.github.com>
2023-05-19 15:12:51 +0100

    Update Current.java

--- src/UI/Current.java
+++ src/UI/Current.java
@@ -2,13 +2,32 @@ package UI;
 
 import java.util.Scanner;
 
-public class Current {  public static final int amount = 100000;
+public class Current {
+    public static final int amount = 100000;
     private static final int maximumAmount = 20000;
 
-
-    public class amount{
-        Scanner input = new Scanner(System.in);
-        int withdrawalAmount = input.nextInt();
-
+    public static class Amount {
+        public void readWithdrawalAmount() {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the withdrawal amount: ");
+            int withdrawalAmount = input.nextInt();
+            
+            if (withdrawalAmount <= maximumAmount) {
+                processWithdrawal(withdrawalAmount);
+            } else {
+                System.out.println("Withdrawal amount exceeds the maximum limit.");
+            }
+        }
+        
+        private void processWithdrawal(int amount) {
+            // Code to process the withdrawal amount can be added here
+            System.out.println("Processing withdrawal of " + amount + "...");
+        }
+    }
+    
+    public static void main(String[] args) {
+        Amount amountObj = new Amount();
+        amountObj.readWithdrawalAmount();
     }
 }
+
