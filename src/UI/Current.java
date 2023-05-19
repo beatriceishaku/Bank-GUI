d986bc856095984e266fcb0e2da42f413ccd7939
Emmanuelowuno <132396131+Emmanuelowuno@users.noreply.github.com>
2023-05-19 15:50:53 +0100

    Update Current.java

--- src/UI/Current.java
+++ src/UI/Current.java
@@ -1,27 +1,45 @@
-package UI;
+ package UI;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Current {
     public static final int amount = 100000;
     private static final int maximumAmount = 20000;
-
+    
     public static class Amount {
+        private static final String FILE_PATH = "withdrawals.txt";
+
         public void readWithdrawalAmount() {
             Scanner input = new Scanner(System.in);
             System.out.print("Enter the withdrawal amount: ");
             int withdrawalAmount = input.nextInt();
-            
-                    private void writeToFile(int amount) {
+
+            if (withdrawalAmount <= maximumAmount) {
+                processWithdrawal(withdrawalAmount);
+                writeToFile(withdrawalAmount);
+            } else {
+                System.out.println("Withdrawal amount exceeds the maximum limit.");
+            }
+        }
+
+        private void processWithdrawal(int amount) {
+            // Code to process the withdrawal amount can be added here
+            System.out.println("Processing withdrawal of " + amount + "...");
+        }
+
+        private void writeToFile(int amount) {
             try (FileWriter writer = new FileWriter(FILE_PATH, true)) {
                 LocalDateTime now = LocalDateTime.now();
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
                 String dateTime = now.format(formatter);
                 String writeUp = "Withdrawal amount: " + amount + ", Date and Time: " + dateTime + "\n";
-                
+
                 writer.write(writeUp);
                 System.out.println("Withdrawal amount and date/time written to file.");
             } catch (IOException e) {
@@ -30,22 +48,6 @@ public class Current {
             }
         }
 
-            if (withdrawalAmount <= maximumAmount) {
-                processWithdrawal(withdrawalAmount);
-            } else {
-                System.out.println("Withdrawal amount exceeds the maximum limit.");
-            }
-        }
-        
-        private void processWithdrawal(int amount) {
-            // Code to process the withdrawal amount can be added here
-            System.out.println("Processing withdrawal of " + amount + "...");
-               } catch (IOException e) {
-                System.out.println("An error occurred while writing to file.");
-                e.printStackTrace();
-            }
-        }
-        
         public void displayFileContents() {
             try {
                 System.out.println("File Contents:");
@@ -57,5 +59,7 @@ public class Current {
             }
         }
     }
+}
+
 
 
