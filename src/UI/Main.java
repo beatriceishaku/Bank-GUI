fea6168fe2b5b37aa2d634bd23b7c8fb977822ae
Beatrice <beatriceishaku45@gmail.com>
2023-05-22 15:03:52 +0100

    Update Main.java

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,8 +1,44 @@
 package UI;
 
-public class Main {  public static void main(String[] args) {
+public class Main {  public static void main(String[] args)throws Exception {
     BankUI bankUI = new BankUI();
     bankUI.mainUI();
+    JLabel label = new JLabel("Place your finger on any alphabet key to scan finger print");
+        JTextField textField = new JTextField();
+        textField.setEditable(false);
+        textField.addKeyListener(new Keychecker());
+
+        JFrame jframe = new JFrame();
+
+        jframe.add(label);
+        jframe.add(textField);
+
+        jframe.setLayout(null);
+        label.setBounds(10,50,360,40);
+
+        jframe.setSize(400, 350);
+
+        jframe.setVisible(true);
+        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+
+    static class Keychecker extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent event) {
+
+            char ch = event.getKeyChar();
+            if (Character.isLetter(ch)) {
+                JOptionPane.showMessageDialog(null, "Fingerprint scan successful");
+                EnterPin enterPin = new EnterPin();
+            } else {
+                JOptionPane.showMessageDialog(null, "Fingerprint scan not successful");
+            }
+
+        }
+
+    }
 
 }
 
