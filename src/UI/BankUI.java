47f452f619abe6e68566b0b469face04766d9431
Beatrice <beatriceishaku45@gmail.com>
2023-05-22 14:51:06 +0100

    Update BankUI.java

--- src/UI/BankUI.java
+++ src/UI/BankUI.java
@@ -3,28 +3,28 @@ package UI;
 import javax.swing.*;
 import java.awt.*;
 
-public class BankUI {
-    private JFrame bankFrame;
-    private JLabel welcomeText;
-    private JTextField inputPin;
-    private JButton login;
-    private JFrame selectionFrame;
-    private JLabel accountChoice;
-    private JButton current;
-    private JButton savings;
-    private JFrame choiceFrame;
-    private JLabel choice;
-    private JFrame currentAccount;
-    private JLabel withdrawC;
-    private JLabel amountWithdraw;
-    private JTextField amountInput;
-    private JButton withdrawBtn;
-    private JTextArea depositC;
-    private JLabel amountDeposit;
-    private JButton depositBtn;
-    private JFrame savingsAccount;
-    private JTextArea withdrawS;
-    private JTextArea depositS;
+public class BankUI extends JFrame{
+    public JFrame bankFrame;
+    public JLabel welcomeText;
+    public JTextField inputPin;
+    public JButton login;
+    public JFrame selectionFrame;
+    public JLabel accountChoice;
+    public JButton current;
+    public JButton savings;
+    public JFrame choiceFrame;
+    public JLabel choice;
+    public JFrame currentAccount;
+    public JLabel withdrawC;
+    public JLabel amountWithdraw;
+    public JTextField amountInput;
+    public JButton withdrawBtn;
+    public JTextArea depositC;
+    public JLabel amountDeposit;
+    public JButton depositBtn;
+    public JFrame savingsAccount;
+    public JTextArea withdrawS;
+    public JTextArea depositS;
 
     private float currentAmount = 0; // Placeholder for the current account balance
     private float savingsAmount = 0; // Placeholder for the savings account balance
@@ -39,6 +39,11 @@ public class BankUI {
         current = new JButton("Current Account");
         savings = new JButton("Savings Account");
         choiceFrame = new JFrame("Action");
+        publicJButton depositButton = new JButton("Deposit");
+        JButton withdrawButton = new JButton("Withdraw");
+        JButton transferButton = new JButton("Transfer");
+        JButton balanceButton = new JButton("Check Balance");
+        
         choice = new JLabel("What action do you want to perform?");
         currentAccount = new JFrame("Current Account");
         withdrawC = new JLabel("you have $"+ Current.amount);
@@ -80,71 +85,35 @@ public class BankUI {
         });
 
         current.addActionListener(e -> {
-            choiceFrame.add(withdrawC);
-            choiceFrame.setLayout(new FlowLayout(FlowLayout.LEFT));
-            choiceFrame.add(withdrawBtn);
-            choiceFrame.add(depositBtn);
+            choiceFrame.add(depositButton);
+            choiceFrame.setLayout(null);
+            choiceFrame.add(withdrawButton);
+            choiceFrame.add(transferButton);
+            choiceFrame.add(balanceButton);
+            depositButton.setBounds(50,100,170,40);
+            withdrawButton.setBounds(250,100,170,40);
+            transferButton.setBounds(50,200,170,40);
+            balanceButton.setBounds(250,200,170,40);
+
             choiceFrame.setVisible(true);
-            choiceFrame.setSize(250, 200);
+            choiceFrame.setSize(500, 450);
             choiceFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         });
 
         savings.addActionListener(e -> {
-            choiceFrame.add(withdrawS);
-            choiceFrame.setLayout(new FlowLayout(FlowLayout.LEFT));
-            choiceFrame.add(withdrawBtn);
-            choiceFrame.add(depositBtn);
+            choiceFrame.add(depositButton);
+            choiceFrame.setLayout(null);
+            choiceFrame.add(withdrawButton);
+            choiceFrame.add(transferButton);
+            choiceFrame.add(balanceButton);
+            depositButton.setBounds(50,100,170,40);
+            withdrawButton.setBounds(250,100,170,40);
+            transferButton.setBounds(50,200,170,40);
+            balanceButton.setBounds(250,200,170,40);
+
             choiceFrame.setVisible(true);
-            choiceFrame.setSize(250, 200);
+            choiceFrame.setSize(500, 450);
             choiceFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        });
-
-        withdrawBtn.addActionListener(e -> {
-            String input = amountInput.getText();
-            try {
-                float withdrawAmount = Float.parseFloat(input);
-                if (withdrawAmount <= 0) {
-                    JOptionPane.showMessageDialog(null, "Invalid amount. Please enter a positive value.");
-                } else if (choiceFrame.getTitle().equals("Current Account")) {
-                    if (withdrawAmount <= currentAmount) {
-                        // Perform withdrawal logic for the current account
-                        currentAmount -= withdrawAmount;
-                        withdrawC.setText("You have $" + currentAmount);
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Insufficient funds in the current account.");
-                    }
-                } else if (choiceFrame.getTitle().equals("Savings Account")) {
-                    if (withdrawAmount <= savingsAmount) {
-                        // Perform withdrawal logic for the savings account
-                        savingsAmount -= withdrawAmount;
-                        withdrawS.setText("You have $" + savingsAmount);
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Insufficient funds in the savings account.");
-                    }
-                }
-            } catch (NumberFormatException nfe) {
-                JOptionPane.showMessageDialog(null, "Invalid amount. Please enter a valid number.");
-            }
-        });
-
-        depositBtn.addActionListener(e -> {
-            String input = amountInput.getText();
-            try {
-                float depositAmount = Float.parseFloat(input);
-                if (depositAmount <= 0) {
-                    JOptionPane.showMessageDialog(null, "Invalid amount. Please enter a positive value.");
-                } else if (choiceFrame.getTitle().equals("Current Account")) {
-                    // Perform deposit logic for the current account
-                    currentAmount += depositAmount;
-                    depositC.setText("You have $" + currentAmount);
-                } else if (choiceFrame.getTitle().equals("Savings Account")) {
-                    // Perform deposit logic for the savings account
-                    savingsAmount += depositAmount;
-                    depositS.setText("You have $" + savingsAmount);
-                }
-            } catch (NumberFormatException nfe) {
-                JOptionPane.showMessageDialog(null, "Invalid amount. Please enter a valid number.");
-            }
         });
     }
 }
